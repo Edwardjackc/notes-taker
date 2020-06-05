e11a4ba9bcb1bb45abdee8458d847480b703bfbe
Edward Cheatham <edwardjackc@gmail.com>
2020-06-05 11:45:56 -0600

    Add subclass Studen

--- oop/person.js
+++ oop/person.js
@@ -33,19 +33,41 @@ class Employee extends Person {
   getBio() {
     return  `${this.firstName} ${this.lastName} is a ${this.position}.`
   }
-  getYearsLeft(){
+  getYearsLeft() {
     return 65 - this.age 
   }
 }
 
+class Student extends Person {
+  constructor(firstName, lastName, age, grade, likes) { 
+    super(firstName, lastName, age, likes)
+    this.grade = grade
+  }
+  updateGrade(change){
+    this.grade += change 
+  }
+  getBio() {
+    const status = this.grade >= 70 ? 'passing' : 'failing'
+    return `${this.firstName} is ${status} the class`
+  }
+}
+
+
 const me  = new Employee('Edward', 'Cheatham', 35,'Student', ['Learning', 'Riding'])
 me.setName('Edward Cheatham')
 console.log(me.getBio())
-console.log(me.getYearsLeft)
+console.log(me.getYearsLeft(2))
 
-const person2 = new Person ('Taiven', 'Rod', 24, ['Teach'])
+
+const person2 = new Person ('Taiven', 'Smith', 24, ['Teach'])
 console.log(person2.getBio())
 
+const person3 = new Student ('Ollie', 'Smith',1, 'Dog', 22)
+person3.updateGrade (- 22)
+console.log(person3.getBio())
 
 
-
+//create class for Student 
+// track student grade 0- 100 
+// override vio to print message pass or fialing , 70 and above 
+// crete updateGrade that takes the amount to add or remove from grade. 
