4402e6effc4b86519938c9a0568369742452eeb0
Edward Cheatham <edwardjackc@gmail.com>
2020-06-06 22:25:48 -0600

    Add hangman app

---
+++ oop/app.js
@@ -0,0 +1,13 @@
+const puzzleEl = document.querySelector('#puzzle');
+const guessEl = document.querySelector('#guesses');
+const game1 = new Hangman('Cat', 2); 
+
+puzzleEl.textContent = game1.puzzleEl
+guessesEl.textContent = game1.statusMessage 
+
+window.addEventListener('keypress', function (e) {
+  const guess = String.fromCharCode(e.charCode)
+  game1.makeGuess(guess)
+  puzzleEl.textContent = game1.getPuzzle()
+  guessesEl.textContent = game1.getStatusMessage()
+})
